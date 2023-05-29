22e9b3ba9e0e238307557db4e71f4bbd3698ef37
Wes Henderson <whendersonii@gmail.com>
2023-05-28 22:12:21 -0500

    Adding configuration methods.

--- src/market-briefs/auth.py
+++ src/market-briefs/auth.py
@@ -16,32 +16,68 @@ class Auth:
         self.smtp_section = "stocks"
 
     def configure_news(self):
-        """Configure the new API."""
-        pass
+        """Configure the News API key."""
+        self.config.read(self.auth_file)
+        print("What is your News API key?")
+        print("    How to get a key: https://newsapi.org/register")
+        api_key = input(": ")
+        if not self.config.has_section(self.news_section):
+            self.config.add_section(self.news_section)
+            self.config.set(self.news_section, "key", api_key)
+
+        with open(self.auth_file, "w", encoding="UTF-8") as file:
+            self.config.write(file)
 
     def configure_stocks(self):
-        """Configure the stocks API."""
-        pass
+        """Configure the Alpha Vantage API key."""
+        self.config.read(self.auth_file)
+        print("What is your Alpha Vantage API key?")
+        print("    How to get a key: https://www.alphavantage.co/support/#api-key")
+        api_key = input(": ")
+        if not self.config.has_section(self.stocks_section):
+            self.config.add_section(self.stocks_section)
+            self.config.set(self.stocks_section, "key", api_key)
+
+        with open(self.auth_file, "w", encoding="UTF-8") as file:
+            self.config.write(file)
 
     def configure_smtp(self):
-        """Configure the SMTP service."""
-        pass
+        """Configure SMTP authentication."""
+        self.config.read(self.auth_file)
+        print("Provide your SMTP information below.")
+        email = input("email: ")
+        password = input("password: ")
+        smtp = input("SMTP Server: ")
+        if not self.config.has_section(self.smtp_section):
+            self.config.add_section(self.smtp_section)
+            self.config.set(self.smtp_section, "email", email)
+            self.config.set(self.smtp_section, "password", password)
+            self.config.set(self.smtp_section, "smtp", smtp)
+
+        with open(self.auth_file, "w", encoding="UTF-8") as file:
+            self.config.write(file)
 
     def auth_stocks(self) -> str:
         """Fetch the API key for the stocks service."""
         try:
             stocks_api_key = self.config[self.stocks_section]["key"]
-            return stocks_api_key
         except KeyError:
-            print("Key error.")
+            print("Must first configure the stocks API.")
+            self.configure_stocks()
+        finally:
+            stocks_api_key = self.config[self.stocks_section]["key"]
+        return stocks_api_key
 
     def auth_news(self) -> str:
         """Fetch the API key for the news service."""
         try:
             news_api_key = self.config[self.news_section]["key"]
-            return news_api_key
         except KeyError:
-            print("Key error.")
+            print("Must first configure the news API.")
+            self.configure_news()
+        finally:
+            news_api_key = self.config[self.news_section]["key"]
+        return news_api_key
 
     def auth_email(self) -> tuple:
         """Fetch the API key for the smtp service."""
@@ -49,6 +85,11 @@ class Auth:
             email = self.config[self.smtp_section]["email"]
             password = self.config[self.smtp_section]["password"]
             server = self.config[self.smtp_section]["smtp"]
-            return email, password, server
         except KeyError:
-            print("Key error.")
+            print("Must first configure an email service.")
+            self.configure_smtp()
+        finally:
+            email = self.config[self.smtp_section]["email"]
+            password = self.config[self.smtp_section]["password"]
+            server = self.config[self.smtp_section]["smtp"]
+        return email, password, server
